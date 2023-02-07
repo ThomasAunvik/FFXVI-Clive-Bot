7dcda05eb23adca98bfc56b51c1444a9770717c3
Thomas Aunvik <contact@thaun.dev>
2023-02-08 00:33:10 +0100

    Web Changes

--- Web/Client/pages/dashboard/index.tsx
+++ Web/Client/pages/dashboard/index.tsx
@@ -1,15 +1,18 @@
-import Head from "next/head"
+import DashboardNavBar from "@/components/DashboardNavBar";
+import Head from "next/head";
 
 const DashboardPage = () => {
-    return <>
-    <Head>
-      <title>Clive Bot - Dashboard</title>
-      <meta name="description" content="Admin Dashboard" />
-    </Head>
-    <main>
-        <h1>Empty Dashboard</h1>
-    </main>
+  return (
+    <>
+      <Head>
+        <title>Clive Bot - Dashboard</title>
+        <meta name="description" content="Admin Dashboard" />
+      </Head>
+      <main>
+        <DashboardNavBar currentPath="/dashboard" />
+      </main>
     </>
-}
+  );
+};
 
 export default DashboardPage;
