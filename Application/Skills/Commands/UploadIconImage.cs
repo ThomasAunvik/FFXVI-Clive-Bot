a43496b98b28dfbd4649e223ac1b94beb25503dd
Thomas Aunvik <contact@thaun.dev>
2023-02-10 02:16:25 +0100

    Form Changes and Azure Blob

---
+++ Application/Skills/Commands/UploadIconImage.cs
@@ -0,0 +1,82 @@
+﻿using CliveBot.Application.Errors;
+using CliveBot.Application.Infrastructure;
+using CliveBot.Azure;
+using CliveBot.Database;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliveBot.Application.Skills.Commands
+{
+    public class UploadIconImage
+    {
+        public class Command : IRequest<SkillDto>
+        {
+            public int SkillId { get; set; }
+            
+            public required IFormFile File { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+
+            }
+        }
+
+        public class Handler : BaseHandler, IRequestHandler<Command, SkillDto>
+        {
+            private readonly AzureUpload _blob;
+
+            public Handler(ApplicationDbContext context, IConfiguration config, AzureUpload blobClient) : base(context, config)
+            {
+                _blob = blobClient;
+            }
+
+            public async Task<SkillDto> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var skill = await _context.Skills
+                    .FirstOrDefaultAsync(s => s.Id == request.SkillId, cancellationToken);
+
+                if (skill == null)
+                {
+                    throw new RestException(HttpStatusCode.NotFound, "Could not find any skill with id: " + request.SkillId);
+                }
+
+                var filePath = $"/images/skill/{skill.Id}/icon.png";
+
+                using var fileStream = request.File.OpenReadStream();
+                var blob = await _blob.Upload(
+                    filePath,
+                    fileStream, 
+                    request.File.ContentType, 
+                    cancellationToken
+                );
+
+                fileStream.Close();
+
+                var iconUrlFilePath = $"cdn;{filePath}";
+                var isSame = skill.IconUrl == iconUrlFilePath;
+                skill.IconUrl = iconUrlFilePath;
+
+                var result = await _context.SaveChangesAsync(cancellationToken);
+                if (result == 0 && !isSame)
+                {
+                    throw new RestException(HttpStatusCode.InternalServerError, "Database failed to save data");
+                }
+
+                return skill.ConvertDto();
+            }
+        }
+    }
+}
