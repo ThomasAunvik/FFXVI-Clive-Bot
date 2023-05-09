4f9ae0aedb5cabbecd0a7eb0a83efebe6e787700
Thomas Aunvik <contact@thaun.dev>
2023-05-10 01:13:13 +0200

    Add 'Terms and Conditions' and 'Privacy Policy' for Reddit App

---
+++ Web/Client/components/legal/redditapp/privacy-policy.ts
@@ -0,0 +1,40 @@
+export const policy = `
+PRIVACY POLICY
+
+Last updated May 10, 2023
+
+
+
+The use of the FFXVI Clive Reddit Appplication is an application used for Moderation of the r/FFXVI (https://www.reddit.com/r/ffxvi).
+
+The other use of FFXVI Clive Reddit Application is to request data from the FFXVI Clive Website (https://ffxvibot.com), to be shown in a comment in Reddit.
+
+Storage of Data:
+
+FFXVI Clive Reddit Application does not store any user data or personal information.
+
+FFXVI Clive Reddit Application when used in r/FFXVI subreddit, and a Post/Comment is published to Reddit, the data (Author, Post Title, Post Body and Post URL) is sent to the r/FFXVI Subreddit Discord trough a Webhook.
+
+User Rights:
+
+If you do request data to be removed from the r/FFXVI Discord Server, related to your Reddit Account, you can do so at: https://discord.gg/aYbmFfg.
+
+Terms of Conditions:
+
+You can read the Terms of Conditions in this page: https://xvibot.com/legal/redditapp/terms-and-conditions
+
+Contact Information:
+
+If there is any other questions, you can send a message at:
+
+Name: Thomas Aunvik
+Phone: +47 41 47 25 56
+Email: contact@thaun.dev
+Github: https://github.com/ThomasAunvik
+
+Discord: Thaun_#0001
+r/FFXVI Discord: https://discord.gg/aYbmFfg
+
+Reddit: u/Thaun_
+r/FFXVI Subreddit Modmail: https://www.reddit.com/message/compose?to=r/ffxvi
+`;
