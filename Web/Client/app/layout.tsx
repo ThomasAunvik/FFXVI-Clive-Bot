af815aa1d0c710cc6291ef4261274781104ac0e0
Thomas Aunvik <contact@thaun.dev>
2024-04-19 23:12:08 +0200

    App Router

---
+++ Web/Client/app/layout.tsx
@@ -0,0 +1,43 @@
+import "@/app/globals.scss";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Clive Bot",
+  description: "Clive Bot, the Final Fantasy XVI Bot for Discord",
+};
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
