d5b3b8bfae9c93992c4542b6219b5c4dab72491c
Thomas Aunvik <contact@thaun.dev>
2024-04-22 23:33:13 +0200

    Remove Filee Usage

--- Web/Client/components/characters/validate.ts
+++ Web/Client/components/characters/validate.ts
@@ -1,21 +1,21 @@
 import * as v from "valibot";
 
 export const characterForm = v.object({
-  name: v.string(),
-  previewFile: v.optional(v.instance(File)),
-  previewImageUrl: v.optional(v.string()),
+	name: v.string(),
+	previewFile: v.optional(v.any()),
+	previewImageUrl: v.optional(v.string()),
 });
 
 export type CharacterFormObj = v.Input<typeof characterForm>;
 
 export const characterVariantForm = v.object({
-  description: v.string(),
-  defaultVariant: v.boolean(),
-  age: v.number([v.integer()]),
-  fromYear: v.number([v.integer()]),
-  toYear: v.number([v.integer()]),
-  previewFile: v.optional(v.instance(File)),
-  previewImageUrl: v.optional(v.string()),
+	description: v.string(),
+	defaultVariant: v.boolean(),
+	age: v.number([v.integer()]),
+	fromYear: v.number([v.integer()]),
+	toYear: v.number([v.integer()]),
+	previewFile: v.optional(v.any()),
+	previewImageUrl: v.optional(v.string()),
 });
 
 export type CharacterVariantFormObj = v.Input<typeof characterVariantForm>;
