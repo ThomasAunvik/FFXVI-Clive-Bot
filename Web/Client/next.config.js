b5e5b45e0f96b789e575d43318c969c64e7224a3
Thomas Aunvik <contact@thaun.dev>
2024-04-25 23:11:07 +0200

    Disable Partial Pre-Rendering

--- Web/Client/next.config.js
+++ Web/Client/next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    ppr: true,
-  },
+	output: "standalone",
+	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
 };
 
 module.exports = nextConfig;
